ce07d9e48e21a4e73d6b1c7ff128f09285cef773
pigeonT <pigeon.tian@gmail.com>
2016-11-09 13:53:08 +0100

    parse init build

--- Parser.ts
+++ Parser.ts
@@ -10,40 +10,54 @@ export class Parser {
     constructor(json : string) {
         this.json = json;
         this.tokens = init(json);
-        this.__build();
     }
-    
-    private __build() : void {
+
+    public parse() : Object {
+        return this.__parseValue();
+    }
+
+    private __parseValue() : Object {
         let cursor : Rule;
-        this.next();
+        this.__next();
         cursor = this.__currentRule;
         switch(cursor.getId()) {
             case TOKEN.LBRACE: 
-                this.next();
-                this.jsonObject = this.getObject();
-                break;
+                this.__next();
+                return this.__getObject();
             default :
-                throw new Error();
+                throw new SyntaxError('json parse exception');
         }               
     }
 
-    private next() : void {
+    private __next() : void {
         this.__currentRule = this.tokens[this.__index];
-        this.__index += 1;
+        this.__index += 1; 
     }
 
-    private getObject() : Object {
+//TODO 
+    private __getObject() : Object {
         let obj : Object = {};
         switch(this.__currentRule.getId()) {
-            case TOKEN.QUOTE :
-                do {
-                    Object.        
-                }
-
+            case TOKEN.QUOTE :{
+                let key = this.__parseString();
+                this.__accept(TOKEN.SEMICOLON);
+                Object.defineProperty(obj, key, {
+                    value : this.__parseValue()
+                });
+                this.__next();
+                return obj;
+            }
             default:
                 throw new SyntaxError('not valid json string');
         }
-        return {};
+    }
+
+    private __parseString() : string {
+        return null;
+    }
+
+    private __accept(token : TOKEN) {
+
     }
 
 }
